f4ca57d3d7635def0757a2c9f662d3739b8883ed
LancemDev <munyaolance1@gmail.com>
2023-09-03 00:21:42 +0300

    :100:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>DoseDock</title>
 </head>
 <body>
-    Hello Koyeb
+    Hello Lizzzy
+    <br>
+    Our project will be hosted here.
+    Database is also on the cloud.
+    I'm so excited about this:)))
+    Authentication is already taken care of. You can check out the links below. This should primarily be the home page but we'll fix that later
+    <a href="{{ route('login') }}">Login</a>
+    <a href="{{ route('register') }}">Register</a>
 </body>
 </html>
